33e118ebfbff77fd5dee07ed122901a20a62fd77
Barış Kahraman <baris-kahra@hotmail.com>
2024-02-01 01:26:22 +0300

    Modal ı kapatabilmek için fonksiyon eklendi. Form gönderildiğinde hangi buton tarafından gönderildiğini öğrenmek için e.submitter özelliği kullanıldı. böylece hangi buton tarafından gönderildiği öğrenilip input değerine bakılacak.

--- assets/js/crowdfunding.js
+++ assets/js/crowdfunding.js
@@ -1,13 +1,36 @@
 const selectRewardBtns = document.querySelectorAll('.select-reward-btn');
+const projectBtn = document.querySelector('#back-to-project');
 const modal = document.querySelector('.modal');
+const closeModalBtn = document.querySelector('.close-modal');
+const form = document.querySelector('form');
+const pledgeInputs = document.querySelectorAll('.pledge-input');
+const continueButtons = document.querySelectorAll('.continue-button');
 
 for (const rewardBtn of selectRewardBtns) {
-    rewardBtn.addEventListener('click',(e) => toggleModal(e))
+    rewardBtn.addEventListener('click',(e) => showModal(e))
 }
 
-modal.showModal();
+projectBtn.addEventListener('click',e => showModal(e))
 
-function toggleModal(e){
+closeModalBtn.addEventListener('click',closeModal)
+
+form.addEventListener('submit',e => sendPledge(e))
+
+function showModal(e){
     e.preventDefault();
     modal.showModal();
+}
+
+function closeModal(){
+    modal.close();
+}
+
+function sendPledge(e){
+    e.preventDefault();
+    let submitBtn = e.submitter;
+    for (let i = 0 ; i < continueButtons.length ; i++) {
+        if(submitBtn == continueButtons[i]){
+            console.log(pledgeInputs[i])
+        }
+    }
 }
